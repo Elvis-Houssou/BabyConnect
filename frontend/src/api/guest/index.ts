d0e260e0d6f1740bbafd17f95f5158ca4b5ce63b
ElvisAfricaDigitalizer <elvis.houssou@africadigitalizer.com>
2026-01-31 17:49:04 +0000

    update guest endpoint

--- frontend/src/api/guest/index.ts
+++ frontend/src/api/guest/index.ts
@@ -1,8 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from 'axios'
 import api from "@/service/api";
-
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
 
 const Guest: any = {};
 
@@ -15,7 +12,7 @@ Guest.get = async () => {
 }
 
 Guest.create = async (data: any) => {
-    return axios.post(`${BASE_URL}/guests/create`, data, {
+    return api.post('/guests/create', data, {
         headers: {
             "Content-Type": "application/json"
         }
